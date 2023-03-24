ce015c9bf6766b696e6394eded544325fbf58844
Ítalo Carvalho Zaina <italo.zaina@bsntecnologia.com.br>
2023-03-24 14:24:41 -0300

    Teste de cifra básica em canção

--- musica.js
+++ musica.js
@@ -1,31 +1,32 @@
-function encontrarCifra(linha) {
-    var reg = /^ *[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b)?)?( +[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *)* *$/,
-        reguniq = /[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *$/,
-        i,
-        tab;
+document.getElementById('ajustar').onclick = function () {
+    const notes = "[CDEFGAB]",
+    accidentals = "(b|bb)?",
+    chords = "(m|maj7|maj|min7|min|sus)?",
+    suspends = "(1|2|3|4|5|6|7|8|9)?",
+    sharp = "(#)?",
+    regex = new RegExp("("+"\\b" + notes + accidentals + chords + suspends + "\\b" + sharp+")", "g");
 
-    if (linha.match(reg)) {
-        i = linha.search(reguniq);
-        tab = encontrarCifra(linha.substr(0, i - 1));
-        tab[i] = linha.substr(i).trim().replace('b', '&');
-        return tab;
-    }
-    return [];
-}
-const reg = /^ *([A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b)?)?( +[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *))* *$/g;
+    const textoMusica = document.getElementsByTagName('textarea')[0].value;
+    let separadoPorLinhas = textoMusica.split('\n');
 
-const textoMusica = document.getElementsByTagName('textarea')[0].value;
-let separadoPorLinhas = textoMusica.split('\n');
-
-alterado = []
-separadoPorLinhas.forEach(linha => {
-    if(linha.match(reg)){
-        alterado.push(linha.replace(reg,'<chord>$1</chord>'));
-    }else{
-        alterado.push(linha);
-    }
-    
-});
-console.log(alterado);
-
-
+    alterado = [];
+    const campo = document.getElementById('resultado');
+    campo.innerHTML = "";
+    separadoPorLinhas.forEach(linha => {
+        if(linha.match(regex)){
+            alterado.push(linha.replace(regex,"<span data-acorde=\"$1\">$1</span>"));
+        }else{
+            alterado.push(linha);
+        }
+        
+    });
+    //monta titulo
+    let h2 = document.createElement('h2');
+    h2.innerHTML = alterado[0];
+    campo.appendChild(h2);
+    alterado.shift();
+    //monta canção
+    let pre = document.createElement('pre');
+    pre.innerHTML = alterado.join('\n');
+    campo.appendChild(pre);
+};
